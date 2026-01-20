4c0aeda8487a772f81b73e2e510942cb6343128b
Tomas Texeira <tom.tex2322@gmail.com>
2026-01-20 17:15:48 +0000

    File changes

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -11,7 +11,7 @@ export default function HeroSection({ content }) {
       {/* Background Image */}
       <div className="absolute inset-0">
         <img
-          src={content?.image_url || "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/696a5cf868f1a8d949987da4/3864d68b9_hero.jpg"}
+          src={content?.image_url || "https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/696a5cf868f1a8d949987da4/d16285113_liftapp.jpg"}
           alt="Josthom Campo"
           className="w-full h-full object-cover" />
 
